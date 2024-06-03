e10362e1538c60614cccb8f389f4f64452c52e1c
Usuario <Usuario@CPD03-M05>
2024-06-03 15:27:09 -0300

    Finalização

---
+++ MauiFlix/Categorias/Aventura.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiFlix.Categorias;
+
+public partial class Aventura : ContentPage
+{
+	public Aventura()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiFlix/Filmes/Fury.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiFlix.Filmes;
+
+public partial class Fury : ContentPage
+{
+	public Fury()
+	{
+		InitializeComponent();
+	}
+}
